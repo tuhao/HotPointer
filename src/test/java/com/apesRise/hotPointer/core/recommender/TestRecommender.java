702cd6be85bf690ca13082ae39caa998a611835d
root <china.violetgo@gmail.com>
2013-12-28 18:31:30 +0000

    init

---
+++ src/test/java/com/apesRise/hotPointer/core/recommender/TestRecommender.java
@@ -0,0 +1,29 @@
+package com.apesRise.hotPointer.core.recommender;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class TestRecommender {
+	public static void main(String args[]) throws Exception {
+
+		DataModel model = new FileDataModel(new File("/usr/local/project/GoWrok/src/github.com/violetgo/HotPointer/HotPointer/test"));// 文件名一定要是绝对路径
+		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+		Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+		List<RecommendedItem> recommendations = recommender.recommend(1, 2);// 为用户1推荐两个ItemID
+		for (RecommendedItem recommendation : recommendations) {
+			System.out.println(recommendation);
+		}
+
+	}
+}
