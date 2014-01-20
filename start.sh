b81cf675b33868f8e0a45c8f4783b58c4f045f4a
root <china.violetgo@gmail.com>
2014-01-20 23:09:38 +0000

    add shell

---
+++ start.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+
+cd /usr/local/hotfood/HotPointer
+/lib/jvm/java-1.7.0-openjdk-1.7.0.25/jre/bin/java -jar /usr/local/hotfood/HotPointer/HotPointer.jar 1&2>> /usr/local/hotfood/HotPointer/send.log
