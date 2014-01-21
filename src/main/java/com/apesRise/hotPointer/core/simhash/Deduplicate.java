af330a5ef35c4f15f78325687b72da514e941765
Ikfly <astro.liuhang@gmail.com>
2014-01-21 23:19:00 +0800

    chineseCharcter

--- src/main/java/com/apesRise/hotPointer/core/simhash/Deduplicate.java
+++ src/main/java/com/apesRise/hotPointer/core/simhash/Deduplicate.java
@@ -1,9 +1,34 @@
 package com.apesRise.hotPointer.core.simhash;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+import com.apesRise.hotPointer.thrift.ThriftClient;
+import com.apesRise.hotPointer.thrift.push_gen.Message;
+
 public class Deduplicate {
 	
 	private static void dedupDB(){
-		
+		ThriftClient client = ThriftClient.getInstance();
+		List<Message> list = client.pullBySort(100, 2);
+		for (Message msg:list){
+			StringReader line = new StringReader(msg.getContent());
+			IKSegmenter segment = new IKSegmenter(line, false);
+			StringBuffer sb = new StringBuffer();
+			try {
+				for(Lexeme lexeme = segment.next();lexeme != null;segment.next()){
+					String word = lexeme.getLexemeText();
+					sb.append(word).append(" ");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
